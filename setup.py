11d210fb80bca519f6c96960f8caf6f04c5afb8d
Pariterre <pariterre@hotmail.com>
2025-03-10 12:04:05 -0400

    Moved dependencies to toml file

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-
-def read_requirements():
-    with open("requirements.txt") as f:
-        return f.read().splitlines()
-
-
-setup(install_requires=read_requirements())
